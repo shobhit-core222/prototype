6f2ebfa3f31b145d2bc9b4477ce872f728d44357
Saha <shobhit.saha@accenture.com>
2025-05-05 15:32:35 +0530

    isr

---
+++ src/app/todos-isr/[name]/page.tsx
@@ -0,0 +1,46 @@
+// import { headers } from "next/headers";
+
+export const revalidate = 100;
+
+interface PageProps {
+  params: {
+    name: string;
+  };
+}
+
+export default async function EdgeHomePage(props: PageProps) {
+  // const headerList = await headers();
+  // const country = headerList.get("x-vercel-ip-country") || "US";
+  const { params } = await props;
+  const { name } = await params;
+  console.log(name);
+  const res = await fetch(
+    `https://prototype.free.beeceptor.com/todos/${name}`,
+    {
+      next: { revalidate: 100 },
+    }
+  );
+  // console.log(res);
+
+  const todos = await res.json();
+  interface todo {
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+  return (
+    <div>
+      <ul>
+        {todos.map(({ id, title, completed }: todo, idx: number) => {
+          return (
+            <li key={idx}>
+              <p>
+                `${id} ${title} ${completed}`
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/todos/[name]/page.tsx
@@ -0,0 +1,41 @@
+// import { headers } from "next/headers";
+
+export const revalidate = 600;
+
+interface PageProps {
+  params: {
+    name: string;
+  };
+}
+
+export default async function EdgeHomePage(props: PageProps) {
+  // const headerList = await headers();
+  // const country = headerList.get("x-vercel-ip-country") || "US";
+  const { params } = await props;
+  const { name } = await params;
+  console.log(name);
+  const res = await fetch(`https://prototype.free.beeceptor.com/todos/${name}`);
+  // console.log(res);
+
+  const todos = await res.json();
+  interface todo {
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+  return (
+    <div>
+      <ul>
+        {todos.map(({ id, title, completed }: todo, idx: number) => {
+          return (
+            <li key={idx}>
+              <p>
+                `${id} ${title} ${completed}`
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
